013f70b7bf591ec9210f273a12d940aa16548638
chaipat <chaipat.trong@gmail.com>
2022-12-15 18:03:21 +0700

    first commit

---
+++ spec/exam111-test-firstapp-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Exam111TestFirstappView from '../lib/exam111-test-firstapp-view';
+
+describe('Exam111TestFirstappView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
